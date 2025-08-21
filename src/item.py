378786909d02d08fc5cc4f2a11bdfc632a3d63c2
ewanp-dev <tpe.dev@proton.me>
2025-08-21 20:53:14 +0100

    added more tests for dependencies

--- src/item.py
+++ src/item.py
@@ -8,6 +8,17 @@ class UsdItem:
     def __init__(self, path: str):
         self.path = path
 
-    def time_test(self):
-        time.sleep(0.1)
-        print("function_sorted")
+    def scrape_dependencies(self):
+        root_layer = Sdf.Layer.FindOrOpen(self.path)
+        all_dependencies = UsdUtils.ComputeAllDependencies(assetPath=self.path)
+        layers = all_dependencies[0]
+        assets = all_dependencies[1]
+        unresolved_paths = all_dependencies[2]
+
+        return layers
+
+
+if __name__ == "__main__":
+    test_file = "/Applications/Houdini/Houdini20.5.445/Frameworks/Houdini.framework/Versions/20.5/Resources/houdini/usd/assets/crag/crag.usd"
+
+    item = UsdItem(path=test_file)
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,10 +1,8 @@
 import time
+from pathlib import Path
 
 import pytest
 import src.item as item
-
-# TODO: Run tests on dependency resolvers
-# TODO: Start writing item class to run tests with
 
 """
 Tests to run:
@@ -14,26 +12,51 @@ Tests to run:
     large vs smaller tests
 """
 
+TESTS_DIR = Path(__file__).parent
+ASSETS_DIR = (TESTS_DIR / "../examples/assets").resolve()
+
+
+def test_asset_loading():
+    usd_file = (ASSETS_DIR / "crag/crag.usd").resolve()
+    assert usd_file.exists()
+
 
 @pytest.fixture
 def usd_item():
     # NOTE: Using dummy usd for now, will switch to a local file at a later point
-    return item.UsdItem(
-        "/Applications/Houdini/Houdini20.5.445/Frameworks/Houdini.framework/Versions/20.5/Resources/houdini/usd/assets/crag/crag.usd"
-    )
+    return item.UsdItem(str((ASSETS_DIR / "rubbertoy/rubbertoy.usd").resolve()))
 
 
+@pytest.mark.skip(reason="Test not relevant")
 def test_path(usd_item):
     # testing that paths match, simple test to see that fixture is working
-    assert (
-        usd_item.path
-        == "/Applications/Houdini/Houdini20.5.445/Frameworks/Houdini.framework/Versions/20.5/Resources/houdini/usd/assets/crag/crag.usd"
-    )
+    assert usd_item.path == str((ASSETS_DIR / "rubbertoy/rubbertoy.usd").resolve())
 
 
-def test_function_runtime(usd_item):
+@pytest.mark.parametrize(
+    "path",
+    [
+        str((ASSETS_DIR / "pig/pig.usd").resolve()),
+        str((ASSETS_DIR / "squab/squab.usd").resolve()),
+    ],
+)
+def test_scrape_dependencies(usd_item, path):
+    # need to see that there are dependencies
+
+    dependencies = item.UsdItem(path)
+    assert len(usd_item.scrape_dependencies()) > 1
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        str((ASSETS_DIR / "pig/pig.usd").resolve()),
+        str((ASSETS_DIR / "squab/squab.usd").resolve()),
+    ],
+)
+def test_scrape_dependencies_runtime(usd_item, path):
     start = time.perf_counter()
-    result = usd_item.time_test()
+    result = item.UsdItem(path).scrape_dependencies()
     elapsed = time.perf_counter() - start
 
-    assert elapsed < 0.5, f"Function too slow: {elapsed:.3f}s"
+    assert elapsed < 1, f"Function too slow: {elapsed:.3f}s"
