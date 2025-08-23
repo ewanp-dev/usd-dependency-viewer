2ac60f89253830186ff566ff675c4714fc7e2d57
ewanp-dev <tpe.dev@proton.me>
2025-08-23 22:22:19 +0100

    added fixtures and set up dependencies test file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,21 @@
+import os
+from pathlib import Path
+
 import pytest
+import src.item as item
+
+# TODO: make a wildcard fixture to pick up all assets for easy parametrize
+
+# ---------------------------------------------------------------
+
+
+@pytest.fixture
+def assets_dir():
+    # NOTE: could also add parametrize mark here to point to multiple assets
+    return (Path(__file_).parent / "../examples/assets").resolve()
+
+
+@pytest.fixture
+def item_instance(assets_dir):
+    # TODO: move to parametrize mark to test multiple files
+    return item.UsdItem(str((assets_dir / "rubbertoy/rubbertoy.usd").resolve()))
---
+++ tests/test_dependencies.py
@@ -0,0 +1,6 @@
+import pytest
+
+"""
+this is where usd dependency related tests will live so the test_item.py
+file doesn't become too bloated
+"""
